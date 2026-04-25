4f28eb21b4f5a9540007b432f07947739e12ca53
jay <roller_pretty.0p@icloud.com>
2026-04-25 09:04:07 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: 
-  // - Use '/aanamichitram/' while viewing on unknown-audio-man.github.io
-  // - Change to '/' once aanami.in is fully connected and working
-  base: '/aanamichitram/', 
+  // Updated to '/' since the site is now live on the custom domain (aanami.in)
+  base: '/', 
   build: {
     target: 'esnext',
     minify: 'esbuild',
