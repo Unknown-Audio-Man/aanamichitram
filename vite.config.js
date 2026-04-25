c222aff6c9316ffafa1e764afd9583beaf9567f0
jay <roller_pretty.0p@icloud.com>
2026-04-25 08:48:59 +0530

    Vite Config update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Setting base to an empty string or './' makes assets relative
-  base: './', 
-})
+  // IMPORTANT: 
+  // - Use '/aanamichitram/' while viewing on unknown-audio-man.github.io
+  // - Change to '/' once aanami.in is fully connected and working
+  base: '/aanamichitram/', 
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: false, 
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          icons: ['lucide-react']
+        }
+      }
+    }
+  }
+});
